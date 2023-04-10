da5a959ba84813ee4b610d1f478e28355c1fe879
ndudar <ndudar@gmail.com>
2023-04-10 14:42:09 -0500

    feat: component shells for Pokemon and PokemonDetail

---
+++ pages/Pokemon/Pokemon.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Pokemon() {
+  return (
+    <div>
+      <h1>I'm the Pokemon main page!</h1>
+    </div>
+  )
+}
---
+++ pages/Pokemon/PokemonDetail.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function PokemonDetail() {
+  return (
+    <div>
+      <h1>I'm the pokemon detail page!</h1>
+    </div>
+  )
+}
