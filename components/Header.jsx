c774d6d9cbb532ea8b5099bd7fea099fdae1419b
ndudar <ndudar@gmail.com>
2023-04-10 15:00:41 -0500

    style: header, footer, html and body

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
+  //styling when link is active
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
   return (
     <header>
-      <Link to="/">Home</Link>
+      <Link to="/" className="site-logo">PokeDex</Link>
       <nav>
-        <NavLink to="pokemon">Pokemon</NavLink>
-        <NavLink to="login">Secret Stuff</NavLink>
+        <NavLink to="pokemon" style={({isActive}) => isActive ? activeStyles : null}>Pokemon</NavLink>
+        <NavLink to="login" style={({isActive}) => isActive ? activeStyles : null}>Secret Stuff</NavLink>
       </nav>
     </header>
   );
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="site-wrapper">
       <Header />
       <main>
         <Outlet />
