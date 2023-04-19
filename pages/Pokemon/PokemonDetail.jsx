d871f066326b6d39c8b8d149cfa9266aabfcf0ba
ndudar <ndudar@gmail.com>
2023-04-19 17:17:46 -0500

    feat: setting up pokemon outlet navlinks

--- pages/Pokemon/PokemonDetail.jsx
+++ pages/Pokemon/PokemonDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useLoaderData, Outlet } from "react-router-dom"
+import { useLoaderData, Outlet, NavLink } from "react-router-dom"
 
 //api
 import { getSinglePokemonWithId } from "../../api"
@@ -12,12 +12,26 @@ export function loader({ params }) {
 export default function PokemonDetail() {
   const pokemon = useLoaderData()
 
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616"
+}
+
   console.log(pokemon)
 
   return (
     <div>
+    <div>
       <h1>{pokemon.name}</h1>
       <img src={pokemon.sprites.front_default} />
+    </div>
+    <nav>
+      <NavLink to="moves" style={({ isActive }) => isActive ? activeStyles : null} >Moves</NavLink>
+      <NavLink to="stats" style={({ isActive }) => isActive ? activeStyles : null} >Stats</NavLink>
+      <NavLink to="evolution" style={({ isActive }) => isActive ? activeStyles : null} >Evolution</NavLink>
+    </nav>
+    <Outlet context={{ pokemon }} />
     </div>
   )
 }
