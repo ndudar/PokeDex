945dc35f048308983551816f40a9880ce5d4047b
ndudar <ndudar@gmail.com>
2023-04-24 16:57:24 -0500

    style: footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,6 +3,6 @@ import React from "react"
 //might be nice to put my links here?
 export default function Footer() {
   return (
-    <footer>I'm the footer!</footer>
+    <footer>&#169; 2023 POKEDEX</footer>
   )
 }
