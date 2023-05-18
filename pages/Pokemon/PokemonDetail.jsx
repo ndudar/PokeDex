e450d75da509a8ca203957b57132c5606969e638
ndudar <ndudar@gmail.com>
2023-05-18 15:05:00 -0500

    fix: state persisting in components

--- pages/Pokemon/PokemonDetail.jsx
+++ pages/Pokemon/PokemonDetail.jsx
@@ -1,18 +1,24 @@
-import React from "react"
-import { useLoaderData, Outlet, NavLink, Link, useLocation } from "react-router-dom"
+import React from "react";
+import {
+  useLoaderData,
+  Outlet,
+  NavLink,
+  Link,
+  useLocation,
+} from "react-router-dom";
 
 //api
-import { getSinglePokemonWithId } from "../../api"
+import { getSinglePokemonWithId } from "../../api";
 
 //loader function
 export function loader({ params }) {
-  return getSinglePokemonWithId(params.id)
+  return getSinglePokemonWithId(params.id);
 }
 
 export default function PokemonDetail() {
-  const pokemon = useLoaderData()
-  const location = useLocation()
-  console.log(location)
+  const pokemon = useLoaderData();
+  const location = useLocation();
+  console.log(location);
 
   //grabbing filter info from location state
   const search = location.state?.search || "";
@@ -21,22 +27,42 @@ export default function PokemonDetail() {
   const activeStyles = {
     fontWeight: "bold",
     textDecoration: "underline",
-    color: "#161616"
-}
+    color: "#161616",
+  };
 
   return (
     <div className="pokemon-detail-container">
-      <Link to={`..${search}`} relative="path" className="back-button">&larr; <span>Back to {type} pokemon</span></Link>
-    <div className="pokemon-detail">
-      <h1>{pokemon.name}</h1>
-      <img src={pokemon.sprites.front_default} />
+      <Link to={`..${search}`} relative="path" className="back-button">
+        &larr; <span>Back to {type} pokemon</span>
+      </Link>
+      <div className="pokemon-detail">
+        <h1>{pokemon.name}</h1>
+        <img src={pokemon.sprites.front_default} />
+      </div>
+      <nav className="pokemon-nav">
+        <NavLink
+          to="moves"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          state={{ search: search, type: type }}
+        >
+          Moves
+        </NavLink>
+        <NavLink
+          to="stats"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          state={{ search: search, type: type }}
+        >
+          Stats
+        </NavLink>
+        <NavLink
+          to="evolution"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          state={{ search: search, type: type }}
+        >
+          Evolution
+        </NavLink>
+      </nav>
+      <Outlet context={{ pokemon }} />
     </div>
-    <nav className="pokemon-nav">
-      <NavLink to="moves" style={({ isActive }) => isActive ? activeStyles : null} >Moves</NavLink>
-      <NavLink to="stats" style={({ isActive }) => isActive ? activeStyles : null} >Stats</NavLink>
-      <NavLink to="evolution" style={({ isActive }) => isActive ? activeStyles : null} >Evolution</NavLink>
-    </nav>
-    <Outlet context={{ pokemon }} />
-    </div>
-  )
+  );
 }
