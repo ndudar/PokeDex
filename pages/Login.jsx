b93dd9e7303bbf2f2298ed702cf303ac037569cd
ndudar <ndudar@gmail.com>
2023-05-22 17:36:37 -0400

    feat: setting up secret form

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,9 +1,19 @@
 import React from "react"
 
+import { Form, useNavigation } from "react-router-dom"
+
 export default function Login() {
+  const navigation = useNavigation()
+
   return (
-    <div>
-      <h1>I'm the login page!</h1>
+    <div className="login-container">
+      <h1>What's the secret password?</h1>
+      <Form className="login-form">
+        <input name="password" type="password" placeholder="Password"/>
+        <button disabled={navigation.state === "submitting"}>
+          {navigation.state === "submitting" ? "Super secret things happening..." : "Knock Knock"}
+        </button>
+      </Form>
     </div>
   )
 }
