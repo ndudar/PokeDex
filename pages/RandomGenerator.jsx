0525abe49ae1e7f49bd0aae5898ebc5d015c811d
ndudar <ndudar@gmail.com>
2023-06-22 12:56:34 -0500

    fix: cleaning up comments and old code in randomizer

--- pages/RandomGenerator.jsx
+++ pages/RandomGenerator.jsx
@@ -1,9 +1,4 @@
 import React, { useState, useEffect } from "react"
-import { defer, useLoaderData, Await, useSearchParams } from "react-router-dom";
-
-//bringing in Pokedex library
-import Pokedex from 'pokedex-promise-v2';
-const P = new Pokedex();
 
 //api
 import { getRandomPokemon } from "../api";
@@ -11,19 +6,12 @@ import { getRandomPokemon } from "../api";
 //components
 import RandomPokemonCard from "../components/RandomPokemonCard";
 
-//grabs random pokemon
-// export function loader() {
-//   return defer({ pokemon: getRandomPokemon() });
-// }
-
 export default function RandomGenerator() {
   const [randomPokemon, setRandomPokemon] = useState(null);
 
   async function fetchRandomPokemon() {
     try {
       const response = await getRandomPokemon();
-      // const pokeName = response.name;
-      // const pokemon = await P.getPokemonByName(pokeName)
       return response
     } catch (err) {
       console.error(err)
@@ -35,26 +23,9 @@ export default function RandomGenerator() {
     .then((response) => setRandomPokemon(response))
   }, [])
 
-  //const dataPromise = useLoaderData();
-
-  // function renderRandomPokemon(pokemon) {
-  //   const displayedPokemon = pokemon;
-  //   console.log(displayedPokemon);
-
-  //   return (
-  //     <div>
-  //       <RandomGenerator displayedPokemon={displayedPokemon} />
-  //     </div>
-  //   )
-
-  // }
-
   return (
-    randomPokemon && 
+    randomPokemon &&
     <div>
-      {/* <React.Suspense fallback={<h2>Loading random pokemon...</h2>}>
-        <Await resolve={dataPromise.pokemon}>{renderRandomPokemon}</Await>
-      </React.Suspense> */}
       <RandomPokemonCard randomPokemon={randomPokemon} />
     </div>
   )
