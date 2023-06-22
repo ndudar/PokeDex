5b26763a3e4856624e5c6618b51e4c0f118cd399
ndudar <ndudar@gmail.com>
2023-06-22 13:12:45 -0500

    feat: setting up battle simulator

---
+++ components/BattleSimulator.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BattleSimulator() {
+  return (
+    <div className="battle-simulator">
+      <div className="poke1">I'm poke 1</div>
+      <div className="poke2">I'm poke 2</div>
+    </div>
+  );
+}
--- pages/Secrets/SecretHome.jsx
+++ pages/Secrets/SecretHome.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 import { useNavigate } from "react-router-dom";
 
+//components
+import BattleSimulator from "../../components/BattleSimulator";
+
 export default function SecretHome() {
   const passwordCheck = localStorage.loggedin
   const navigate = useNavigate()
@@ -23,6 +26,7 @@ export default function SecretHome() {
       <div>
         <h1>I'm secret stuff!</h1>
         <button onClick={() => handleLogout()}>Log Out</button>
+        <BattleSimulator />
       </div>
     )
   }
