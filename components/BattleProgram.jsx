87cad83f3770237dd95a914191bc0f287569f112
ndudar <ndudar@gmail.com>
2023-06-23 16:26:44 -0500

    feat: hp updating for both pokes in battle

--- components/BattleProgram.jsx
+++ components/BattleProgram.jsx
@@ -19,45 +19,59 @@ export default function BattleProgram(props) {
   } = props;
 
   const [currentMove, setCurrentMove] = useState(null);
-  const [battleText, setBattleText] = useState(`The pokemon are ready to battle!`)
+  const [battleText, setBattleText] = useState(
+    `The pokemon are ready to battle!`
+  );
   const [currentPokemon, setCurrentPokemon] = useState(poke1Name);
-  const [battleButtonText, setBattleButtonText] = useState(`Let's battle!`)
-
-  let poke1CurrentHP = poke1HP;
-  let poke2CurrentHP = poke2HP;
+  const [battleButtonText, setBattleButtonText] = useState(`Let's battle!`);
+  const [poke1CurrentHP, setPoke1CurrentHP] = useState(poke1HP);
+  const [poke2CurrentHP, setPoke2CurrentHP] = useState(poke2HP);
 
   async function handleClick() {
     const randomMove =
       currentPokemon === poke1Name
         ? poke1Moves[Math.floor(Math.random() * poke1Moves.length)]
         : poke2Moves[Math.floor(Math.random() * poke2Moves.length)];
-    console.log(randomMove);
+    const randomMovePromise = await P.getMoveByName(randomMove.move.name);
+    setCurrentMove(randomMovePromise);
     setBattleText(`${currentPokemon} uses ${randomMove.move.name}!`);
     setCurrentPokemon(currentPokemon === poke1Name ? poke2Name : poke1Name);
-    setBattleButtonText(poke1CurrentHP <= 0 || poke2CurrentHP <= 0 ? `Battle Over!` : `Next Move!`);
+
+    const hit = randomMovePromise.pp;
+    if (currentPokemon === poke1Name) {
+      setPoke2CurrentHP((prevHP) => prevHP - calculateDamage(poke1Attack, poke2Defense, hit));
+    } else {
+      setPoke1CurrentHP((prevHP) => prevHP - calculateDamage(poke2Attack, poke1Defense, hit));
+    }
+
+    setBattleButtonText(
+      poke1CurrentHP <= 0 || poke2CurrentHP <= 0 ? `Battle Over!` : `Next Move!`
+    );
   }
 
-  //need func for selecting random move and grabbing info from pokedex
-  //need handle click func for stepping through battle
-  //need mathematical formulas for calculating hit to HP
+  // useEffect(() => {
+  //   const hit = currentMove?.pp;
+  //   if (currentPokemon === poke1Name) {
+  //     setPoke2CurrentHP(poke2CurrentHP - calculateDamage(poke1Attack, poke2Defense, hit));
+  //   } else {
+  //     setPoke1CurrentHP(poke1CurrentHP - calculateDamage(poke2Attack, poke1Defense, hit));
+  //   }
+  // }, [currentMove, currentPokemon]);
 
-  //according to Pokemon Go:
-  //divide attack by defense -> get mod
-  //0.5 * power, then * attack/defense, then * mod
-  //+ 1 to the above = HP lost
-
-  //to get to a move name or url: poke1Moves[0].move.name/url
-  //P.getMoveByName("pound");
-  //or if make API call traditionally by URL, then:
-  //returned obj.power or pp? we can decide depending on how long/short battles are
+  //this is a formula I'm making up that gives the battle a good pace
+  function calculateDamage(attack, defense, power) {
+    const quotient = attack / defense;
+    const mod = attack % defense;
+    return Math.round(power * quotient + mod / 3);
+  }
 
   return (
     <div>
       <h3>Here's the battle program</h3>
       <p>Team 1: {poke1Name}</p>
       <p>Team 2: {poke2Name}</p>
-      <p>Team 1 HP: {poke1CurrentHP}</p>
-      <p>Team 2 HP: {poke2CurrentHP}</p>
+      <p>Team 1 HP: {poke1CurrentHP ? poke1CurrentHP : poke1HP}</p>
+      <p>Team 2 HP: {poke2CurrentHP ? poke2CurrentHP : poke2HP}</p>
       <p>Team 1 Attack: {poke1Attack}</p>
       <p>Team 2 Attack: {poke2Attack}</p>
       <p>Team 1 Defense: {poke1Defense}</p>
